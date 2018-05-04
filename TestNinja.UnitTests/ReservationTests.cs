95b29e4bc0c62e21bf4dfdc7ad53c5fb8e4e9684
bradonf333 <bradonf123@gmail.com>
2018-05-03 19:39:01 -0600

    Test all scenarios for the Reservation class

--- TestNinja.UnitTests/ReservationTests.cs
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -19,5 +19,50 @@ namespace TestNinja.UnitTests
             // Assert
             Assert.IsTrue(result);
         }
+
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsNotAdmin_ShouldReturnFalse()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = false });
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_ReservationMadeBySameUser_ShouldReturnTrue()
+        {
+            // Arrange
+            var reservation = new Reservation();
+            var user = new User();
+            user.IsAdmin = false;
+            reservation.MadeBy = user;
+
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_ReservationMadeByDifferentUser_ShouldReturnFalse()
+        {
+            // Arrange
+            var reservation = new Reservation();
+            var user = new User();
+            user.IsAdmin = false;
+            reservation.MadeBy = user;
+
+            // Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = false });
+
+            // Assert
+            Assert.IsFalse(result);
+        }
     }
 }
