f1966ceecfa57913916c04ae15137b95a8cc29b8
bradonf333 <bradonf123@gmail.com>
2018-05-03 20:56:38 -0600

    Test Add and Max methods

---
+++ TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        Math math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            math = new Math();
+        }
+
+
+        [Test]
+        public void Add_ShouldAddTwoNumbers_ShouldReturnTrue()
+        {
+            var result = math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstNumberIsGreater_ShouldReturnTrue()
+        {
+            var result = math.Max(3, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_SecondNumberIsGreater_ShouldReturnTrue()
+        {
+            var result = math.Max(2, 3);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_BothNumbersAreEqual_ShouldReturnTrue()
+        {
+            var result = math.Max(2, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
