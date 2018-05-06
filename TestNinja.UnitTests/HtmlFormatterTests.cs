e32fde85307cff4bafbef281854467217d2201ee
bradonf333 <bradonf123@gmail.com>
2018-05-06 09:47:35 -0600

    HtmlFormatterTests

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_EncloseTheStringWithStrongElement_ShouldReturnTrue()
+        {
+            var htmlFormatter = new HtmlFormatter();
+
+            var result = htmlFormatter.FormatAsBold("This is bold text!");
+
+            Assert.That(result, Is.EqualTo("<strong>This is bold text!</strong>").IgnoreCase);
+        }
+    }
+}
