6474b7f2dd4dfebbab847d20c6d4b60ba211f70c
bradonf333 <bradonf123@gmail.com>
2018-05-06 15:48:56 -0600

    Dependency Injection via Properties

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ShouldReturnError()
+        {
+            var sut = new VideoService();
+            sut.FileReader = new FakeFileReader();
+
+            var result = sut.ReadVideoTitle();
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
